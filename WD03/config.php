526ca57cd8a062231426a260a1aff505f8cb8089
Sergey <verstka.sv@mail.rugit config --global user.email verstka.sv@mailgit config --global user.email verstka.sv@mail.ru>
2018-07-11 10:12:11 +0300

    ferst commit

---
+++ WD03/config.php
@@ -0,0 +1,16 @@
+<?php 
+
+// ВD SETTINGS
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'WD03');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+// Хост сайта
+define('HOST', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/');
+
+// Физический путь к корневой директории скрипта
+define('ROOT', dirname(__FILE__).'/');
+
+
+?>
